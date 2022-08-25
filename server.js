7a509ceea052dccd0dc1f5efa2788f99b00d0b9f
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-08-25 13:06:26 +0100

    fix: Correctly served static files.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use('https://leaf-lore.herokuapp.com/static', express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res)=>{
   res.sendFile(path.join(__dirname, "client/build",'index.html'));
 })
