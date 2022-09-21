10360d0058b8656c09613a8a2f6989a8b11a6789
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-09-21 16:25:47 +0100

    feat: Added logo and header structure

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,7 +1,15 @@
-import React from "react"
+ import React from "react"
 
 export default function Header() {
   return (
-    <h1>Header</h1>
+    <div className="nav-wrapper">
+      <img src="../images/logo.webp"/> {/* Modified from: https://www.vecteezy.com/free-vector/plant-logo */}
+
+      <ul>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+      </ul>      
+    </div>
   )
 };
