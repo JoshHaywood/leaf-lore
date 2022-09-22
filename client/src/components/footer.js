fb9e3507f44235efef4fcfa2c4c9720e647e088c
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-09-22 16:33:14 +0100

    feat: Creater footer component. refactor(header) Adjusted links to use react "Link".

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,7 +1,42 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <h1>Footer</h1>
+    <footer className="footer-container">
+      <div className="footer-content">
+        {/* Links */}
+        <div className="footer-links">
+          <span>Pages</span>
+          
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/results">Results</Link></li>
+          </ul>
+        </div>
+
+        {/* Search Bar */}
+        <div className="search-bar-container">
+          <span>Results</span>
+
+          <div className="search-input-container">
+            <input type="text" placeholder="Search"/>
+          
+            <div className="flex">
+              <button type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"> {/* Attribution: https://fontawesome.com/icons/magnifying-glass?s=solid&f=classic */}
+                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Footnote */}
+      <div className="footnote-wrapper">
+        <a href="https://www.joshhaywood-portfolio.com">© Josh Haywood {new Date().getFullYear()}</a>
+      </div>
+    </footer>
   )
 };
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,5 @@
- import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -13,8 +14,8 @@ export default function Header() {
         {/* Nav links */}
         <div className="nav-links-container">
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/results">Results</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/results">Results</Link></li>
           </ul>
 
           {/* Search Bar */}
